1b35e1251c636badb4a87c676779236b4225fbfe
Veronica Pichay <veronicapichay@outlook.com>
2022-10-19 13:50:07 -0700

    Lab 10.2 - Try-Catch

--- Lab10.1-HR/src/com/hr/personnel/HourlyEmployee.java
+++ Lab10.1-HR/src/com/hr/personnel/HourlyEmployee.java
@@ -8,6 +8,8 @@
 
 package com.hr.personnel;
 
+import gov.irs.IllegalWageException;
+
 import java.time.LocalDate;
 
 public class HourlyEmployee extends Employee {
@@ -26,7 +28,7 @@ public class HourlyEmployee extends Employee {
         super(name, hireDate);  // delegate to superclass ctor for name, hireDate
     }
 
-    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) throws IllegalArgumentException {
+    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) throws IllegalWageException {
         this(name, hireDate);   // delegate to neighboring ctor for name, hireDate
         setRate(rate);          // handle rate here, by delegating to setter
         setHours(hours);        // handle hours here, by delegating to setter
@@ -49,9 +51,9 @@ public class HourlyEmployee extends Employee {
         return rate;
     }
 
-    public void setRate(double rate) {
+    public void setRate(double rate) throws IllegalWageException {
         if (rate < FEDERAL_MINIMUM_WAGE) {
-            throw new IllegalArgumentException("Illegal amount: " + rate + " Federal minimum wage is: " + FEDERAL_MINIMUM_WAGE);
+            throw new IllegalWageException("Illegal amount: " + rate + " Federal minimum wage is: " + FEDERAL_MINIMUM_WAGE);
         }
         else {
             this.rate = rate;
--- Lab10.1-HR/src/com/hr/personnel/client/HRClient.java
+++ Lab10.1-HR/src/com/hr/personnel/client/HRClient.java
@@ -12,6 +12,8 @@ import com.hr.personnel.Department;
 import com.hr.personnel.Employee;
 import com.hr.personnel.HourlyEmployee;
 import com.hr.personnel.SalariedEmployee;
+import gov.irs.IllegalWageException;
+
 import java.time.LocalDate;
 
 /**
@@ -19,17 +21,22 @@ import java.time.LocalDate;
  */
 class HRClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         // create Department object
         Department dept = new Department("Sales", "Seattle");
         System.out.println(dept);
 
         // add Employees to it
-        dept.addEmployee(new SalariedEmployee("Jason",  LocalDate.of(1990, 8, 24), 2250.0));
-        dept.addEmployee(new HourlyEmployee("Julie",    LocalDate.of(2000, 2, 2), 35.0, 30.0));
-        dept.addEmployee(new SalariedEmployee("Amilia", LocalDate.of(1999, 2, 6), 1250.0));
-        dept.addEmployee(new HourlyEmployee("Logan",    LocalDate.of(2002, 6, 2), 40.0, 40.0));
-
+        try {
+            dept.addEmployee(new SalariedEmployee("Jason", LocalDate.of(1990, 8, 24), 2250.0));
+            dept.addEmployee(new SalariedEmployee("Amilia", LocalDate.of(1999, 2, 6), 1250.0));
+            dept.addEmployee(new HourlyEmployee("Julie", LocalDate.of(2000, 2, 2), 50.0, 30.0));
+            dept.addEmployee(new HourlyEmployee("Logan", LocalDate.of(2002, 6, 2), 0.0, 40.0));
+       }
+        catch (IllegalWageException e) {
+           e.printStackTrace();
+           System.out.println(" Error " + e);
+       }
         // list its Employees
         System.out.println("\nList employees:");
         dept.listEmployees();
