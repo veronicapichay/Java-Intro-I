1d378d23c466400ee15e5e242ba30bf576534af7
Veronica Pichay <veronicapichay@outlook.com>
2022-10-20 10:59:46 -0700

    Options for try-catch-finally

---
+++ Lab10-Extra/src/com/transportation/BoatClient.java
@@ -0,0 +1,9 @@
+package com.transportation;
+
+class BoatClient {
+
+    public static void main(String[] args) {
+        Boat boat1 = new Boat("BBB");
+        boat1.moveTo("Palo Alto");
+    }
+}
